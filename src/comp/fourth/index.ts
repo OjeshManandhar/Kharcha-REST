3075074d04934b7ae7146ff483b46b3124ed3110
OjeshManandhar <ozes.manandhar@gmail.com>
2021-05-17 18:02:25 +0545

    Add dummy code

---
+++ src/comp/fourth/index.ts
@@ -0,0 +1,7 @@
+import square from 'comp/square';
+
+function fourth(num: number): number {
+  return square(num) * square(num);
+}
+
+export default fourth;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import fourth from 'comp/fourth';
+
+const str = 'This is a string';
+
+console.log('str:', str);
+console.log('fourth(3):', fourth(3));
