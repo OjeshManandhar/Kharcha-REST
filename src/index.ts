6f476a9bf446424aa3e38d5417ee069e18203afc
OjeshManandhar <ozes.manandhar@gmail.com>
2021-05-18 08:29:44 +0545

    Setup a simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('this is a string');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// Request parser
+app.use(bodyParser.json());
+
+app.use('/', (req, res, next) => {
+  console.log('req.url:', req.url);
+  console.log('req.body:', req.body);
+
+  res.send('Hello world!');
+});
+
+app.listen(5000, () => {
+  console.log('Server started at port 5000');
+});
