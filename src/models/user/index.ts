a5ee5e0879c371b3e6d6691b973193138b969f69
OjeshManandhar <ozes.manandhar@gmail.com>
2021-05-19 14:22:54 +0545

    Add validation in User model

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -5,8 +5,15 @@ import { Schema, model } from 'mongoose';
 import type { IUser } from './types';
 
 const schema = new Schema<IUser>({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: [4, 'Must be at least 4, got {VALUE}'],
+    maxLength: [15, 'Can be at most 15, got {VALUE}']
+  },
+  password: { type: String, required: true, trim: true },
   tags: { type: [String], required: true, default: [] }
 });
 
