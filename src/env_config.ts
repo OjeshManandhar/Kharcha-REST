dc6da47e9368bdc34aebd7ab2287524032173de6
OjeshManandhar <ozes.manandhar@gmail.com>
2021-05-19 18:41:59 +0545

    Add JWT_SECRET in .env

--- src/env_config.ts
+++ src/env_config.ts
@@ -3,3 +3,5 @@ export const PORT = process.env.PORT;
 export const MONGO_DB = process.env.MONGO_DB;
 export const MONGO_USER = process.env.MONGO_USER;
 export const MONGO_PASS = process.env.MONGO_PASS;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
