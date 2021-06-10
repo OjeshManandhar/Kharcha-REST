87a05d111c712e049664f12f11801aec3e311ed7
OjeshManandhar <ozes.manandhar@gmail.com>
2021-06-10 09:23:12 +0545

    Add simple test

---
+++ test/isAuth.test.ts
@@ -0,0 +1,23 @@
+// packages
+import { expect } from 'chai';
+
+// middleware
+import isAuth from './../src/middleware/isAuth';
+
+// types
+import type { Request, Response, NextFunction } from 'express';
+
+describe('Authorization middleware', () => {
+  const mockReq: Partial<Request> = {
+    headers: {
+      Authorization: 'Bearer qwertyuiop'
+    }
+  };
+  const mockRes: Partial<Response> = {};
+  const mockNext: Partial<NextFunction> = () => {};
+
+  it('should have property isAuth: false', () => {
+    isAuth(mockReq as Request, mockRes as Response, mockNext as NextFunction);
+    expect(mockReq).to.have.property('isAuth', false);
+  });
+});
