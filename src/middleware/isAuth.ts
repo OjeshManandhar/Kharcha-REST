9c2af2c0ddc6d387ce7d71623edc7d0148118a0e
OjeshManandhar <ozes.manandhar@gmail.com>
2021-06-12 13:29:19 +0545

    Set req.isAuth to default to false in isAuth

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -17,21 +17,20 @@ import type { Request, Response, NextFunction } from 'express';
 export default (req: Request, res: Response, next: NextFunction): void => {
   const authHeader = req.headers?.authorization;
 
+  req.isAuth = false;
+
   if (!authHeader) {
-    req.isAuth = false;
     next();
     return;
   }
 
   const token = authHeader.split(' ')[1];
   if (!token) {
-    req.isAuth = false;
     next();
     return;
   }
 
   if (!JWT_SECRET) {
-    req.isAuth = false;
     next(new CustomError('JWT error'));
     return;
   }
@@ -39,8 +38,6 @@ export default (req: Request, res: Response, next: NextFunction): void => {
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       const expiredAt = (err as { expiredAt: Date | undefined }).expiredAt;
-
-      req.isAuth = false;
 
       next(
         new CustomError('Invalid Token', 401, [
@@ -57,11 +54,10 @@ export default (req: Request, res: Response, next: NextFunction): void => {
     const decodedToken = decoded as Token;
 
     if (decodedToken && decodedToken._id) {
-      req.userId = mongoose.Types.ObjectId(decodedToken._id);
       req.isAuth = true;
+      req.userId = mongoose.Types.ObjectId(decodedToken._id);
       next();
     } else {
-      req.isAuth = false;
       next();
     }
   });
