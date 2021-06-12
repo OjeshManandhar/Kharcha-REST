f968f378c5b729c4d433add27ce9478185380fcc
OjeshManandhar <ozes.manandhar@gmail.com>
2021-06-12 18:07:49 +0545

    Add test for CustomError

---
+++ test/utils/customError.test.ts
@@ -0,0 +1,44 @@
+// packages
+import { expect } from 'chai';
+
+// utils
+import CustomError, { ErrorData } from './../../src/utils/customError';
+
+describe('[customError] Custom Error class utility', () => {
+  it('should return all default fields with nothing is passed', () => {
+    const DEFAULT = {
+      message: 'An error occured',
+      status: 500,
+      data: [] as ErrorData
+    };
+    const err = new CustomError();
+
+    expect(err).to.be.instanceOf(CustomError);
+    expect(err).to.have.property('message', DEFAULT.message);
+    expect(err).to.have.property('status', DEFAULT.status);
+    expect(err).to.have.deep.property('data', DEFAULT.data);
+  });
+
+  it('should return all fields with given arguments when parameters are passed', () => {
+    const args = {
+      message: 'Dummy Error',
+      status: 401,
+      data: [
+        {
+          message: 'Dummy message',
+          field: 'dummy'
+        },
+        {
+          message: 'Dummy message 2',
+          field: 'dummy 2'
+        }
+      ] as ErrorData
+    };
+    const err = new CustomError(args.message, args.status, args.data);
+
+    expect(err).to.be.instanceOf(CustomError);
+    expect(err).to.have.property('message', args.message);
+    expect(err).to.have.property('status', args.status);
+    expect(err).to.have.deep.property('data', args.data);
+  });
+});
