674a95e663c1c4964d2dbf62bd1516c241b3c840
OjeshManandhar <ozes.manandhar@gmail.com>
2021-06-13 21:03:33 +0545

    Write 1 test for createUser

--- test/resolvers/users.test.ts
+++ test/resolvers/users.test.ts
@@ -2,14 +2,14 @@
 import { expect } from 'chai';
 
 // gql
-import * as users from './../../src/gql/resolvers/users';
+import * as users from 'gql/resolvers/users';
 
 // utils
-import CustomError from './../../src/utils/customError';
+import CustomError from 'utils/customError';
 
 // types
 import type { Request } from 'express';
-import type * as T from './../../src/gql/resolvers/users/types';
+import type * as T from 'gql/resolvers/users/types';
 
 describe('[users] User resolver', async () => {
   describe('[createUser]', () => {
@@ -28,21 +28,28 @@ describe('[users] User resolver', async () => {
       mockArgs.confirmPassword = 'password';
     });
 
-    it("should throw CustomError('Unauthorized. Log out first', 401) if req.isAuth = true i.e. logged in", () => {
-      mockReq.isAuth = false;
+    it("should throw CustomError('Unauthorized. Log out first', 401) if req.isAuth = true i.e. logged in", done => {
+      mockReq.isAuth = true;
 
       users
         .createUser(mockArgs, mockReq as Request)
         .then(result => {
           expect(result).to.be.undefined;
+
+          done();
         })
         .catch(err => {
+          console.log('err:', err.message);
+
           expect(err).to.be.instanceOf(CustomError);
           expect(err).to.have.property(
             'message',
             'Unauthorized. Log out first'
           );
           expect(err).to.have.property('status', 401);
+          expect(err).to.have.property('data').that.is.empty;
+
+          done();
         });
     });
   });
