cfb75f6f89b91fd9e3fa8651b137af54385657c7
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-03 09:56:56 +0545

    Write tests for generateQuery

---
+++ test/utils/generateQuery.test.ts
@@ -0,0 +1,47 @@
+// packages
+import { expect } from 'chai';
+
+// utils
+import generateQuery from 'utils/generateQuery';
+
+describe("[generateQuery] Query generator for filterRecords (Using 'numbers' only for this tes)", () => {
+  it('should only return the start/end value when both are given and same', () => {
+    const start = 5,
+      end = start;
+
+    expect(generateQuery(start, end)).to.be.equal(start);
+    expect(generateQuery(start, end)).to.be.equal(end);
+  });
+
+  it('should return object with $gte and $lte when both start and end are given but not same', () => {
+    const start = 5,
+      end = 10;
+
+    expect(generateQuery(start, end)).to.be.deep.equal({
+      $gte: start,
+      $lte: end
+    });
+  });
+
+  it('should return object with only $gte when only start is given', () => {
+    const start = 5;
+
+    const result = generateQuery(start, null);
+
+    expect(result).to.have.property('$gte', start);
+    expect(result).to.not.have.property('$lte');
+  });
+
+  it('should return object with only $lte when only end is given', () => {
+    const end = 5;
+
+    const result = generateQuery(null, end);
+
+    expect(result).to.not.have.property('$gte');
+    expect(result).to.have.property('$lte', end);
+  });
+
+  it('should return null when both start and end are not given', () => {
+    expect(generateQuery(null, null)).to.be.null;
+  });
+});
